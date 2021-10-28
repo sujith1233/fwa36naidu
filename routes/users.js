4a3910b774e3b1ab10cbf1033c8e9cb44bde6f3b
sujith1233 <S545436@NWMISSOURI.EDU>
2021-10-27 23:23:27 -0500

    changes in users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var visits = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  visits++;
+  res.send('No of visits: '+visits);
 });
 
 module.exports = router;
